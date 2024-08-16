5620c5def2acd3830e3e6fcdda4f91bbd18ed751
night-fury-3 <jonathonfrakes3@gmail.com>
2024-08-16 18:52:20 -0400

    5. Enums Unleashed Pattern Matching and Options
    feat: Declaring Enum Values

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,17 @@ fn main() {
         title: String::from("An Audiobook"),
     };
 
-    print_media(audiobook)
+    let good_movie = Media::Movie {
+        title: String::from("Good Moive"),
+        director: String::from("Good Director"),
+    };
+
+    let bad_book = Media::Book {
+        title: String::from("Bad Book"),
+        author: String::from("Bad Author"),
+    };
+
+    print_media(good_movie);
+    print_media(bad_book);
+    print_media(audiobook);
 }
